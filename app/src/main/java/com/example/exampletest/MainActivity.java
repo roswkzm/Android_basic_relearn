93315a8fe6da4b329e835b07dc9601ed79c5818b
이진영 <roswkzm@naver.com>
2022-01-21 13:06:00 +0900

    Intent

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.exampletest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,22 +10,28 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText et_id;
-    Button btn_test;
+    private EditText et_text;
+    private Button btn_test;
+    private Button btn_move;
+    private String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_id = findViewById(R.id.et_id);
+        et_text = findViewById(R.id.et_text);
         btn_test = findViewById(R.id.btn_test);
+        btn_move = findViewById(R.id.btn_move);
 
-        btn_test.setOnClickListener(new View.OnClickListener() {
+        btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                et_id.setText("이진영1");
+                str = et_text.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("str",str);
+                startActivity(intent);  //엑티비티 이동
             }
-       });
+        });
     }
 }
