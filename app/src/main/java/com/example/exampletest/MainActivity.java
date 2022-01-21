721b7e669598dca0de157052ed8f95f4c283b3c8
이진영 <roswkzm@naver.com>
2022-01-21 13:34:16 +0900

    ImageView + ToastMessage

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_test;
     private Button btn_move;
     private String str;
+    private ImageView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +26,22 @@ public class MainActivity extends AppCompatActivity {
         et_text = findViewById(R.id.et_text);
         btn_test = findViewById(R.id.btn_test);
         btn_move = findViewById(R.id.btn_move);
+        test = findViewById(R.id.test);
 
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 str = et_text.getText().toString();
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                intent.putExtra("str",str);
+               intent.putExtra("str",str);
                 startActivity(intent);  //엑티비티 이동
+            }
+        });
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"이진영이진영이진영이진영",Toast.LENGTH_SHORT).show();
             }
         });
     }
