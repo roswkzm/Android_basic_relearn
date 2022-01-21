4725d62e3ff402f3d3e0096a6fa7e4793cc18164
이진영 <roswkzm@naver.com>
2022-01-21 14:10:07 +0900

    ListView

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -5,44 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et_text;
-    private Button btn_test;
-    private Button btn_move;
-    private String str;
-    private ImageView test;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_text = findViewById(R.id.et_text);
-        btn_test = findViewById(R.id.btn_test);
-        btn_move = findViewById(R.id.btn_move);
-        test = findViewById(R.id.test);
+        list = (ListView) findViewById(R.id.list);
 
-        btn_move.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                str = et_text.getText().toString();
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
-               intent.putExtra("str",str);
-                startActivity(intent);  //엑티비티 이동
-            }
-        });
+        List<String> data = new ArrayList<>();
 
-        test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"이진영이진영이진영이진영",Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        data.add("이진영1");
+        data.add("이진영2");
+        data.add("이진영3");
+        adapter.notifyDataSetChanged();
+    };
 }
