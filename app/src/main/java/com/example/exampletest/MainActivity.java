38fde14bfd81bbc50c1a4064e4a3b82a7ca188af
이진영 <roswkzm@naver.com>
2022-01-21 15:50:33 +0900

    WebView

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,29 +20,37 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et_save;
-    String shared = "file";
+    private WebView webView;
+    private String url = "https://www.naver.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_save = findViewById(R.id.et_save);
-
-        SharedPreferences sharedPreferences = getSharedPreferences(shared,0);
-        String value = sharedPreferences.getString("Lee","");
-        et_save.setText(value);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClientClass());
     };
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
 
-        SharedPreferences sharedPreferences = getSharedPreferences(shared,0);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        String value = et_save.getText().toString();
-        editor.putString("Lee",value);
-        editor.commit();
+        if((keyCode==KeyEvent.KEYCODE_BACK) && webView.canGoBack()){
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+
+            return true;
+        }
     }
 }
