cbf1098f0652fc0ef161e54e3bc5eec34b505072
이진영 <roswkzm@naver.com>
2022-01-25 19:50:36 +0900

    Service(백그라운드 음악재생)

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -17,44 +18,28 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_dialog;
-    TextView tv_result;
+    Button btn_start, btn_stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_dialog = (Button) findViewById(R.id.btn_dialog);
-        tv_result = (TextView) findViewById(R.id.tv_result);
 
-        btn_dialog.setOnClickListener(new View.OnClickListener() {
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
-                ad.setIcon(R.mipmap.ic_launcher);
-                ad.setTitle("제목");
-                ad.setMessage("메시지 부분");
+                startService(new Intent(getApplicationContext(),MusicService.class));
+            }
+        });
 
-                final EditText et = new EditText(MainActivity.this);
-                ad.setView(et);
-
-                ad.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        String result = et.getText().toString();
-                        tv_result.setText(result);
-                        dialogInterface.dismiss();
-                    }
-                });
-
-                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                });
-                ad.show();
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(getApplicationContext(),MusicService.class));
             }
         });
     }
