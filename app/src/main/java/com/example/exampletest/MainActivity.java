3faab373b38d21d4698b22309c1fb99f5e05dbb8
이진영 <roswkzm@naver.com>
2022-01-26 19:15:00 +0900

    프로젝트 초기화

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -12,80 +12,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;  //바텀 네비게이션 뷰
-    private FragmentManager fm;
-    private FragmentTransaction ft;
-    private Frag1 frag1;
-    private Frag2 frag2;
-    private Frag3 frag3;
-    private Frag4 frag4;
-    private Frag5 frag5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = findViewById(R.id.bottomNavi);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.action_airplane1:
-                        setFrag(0);
-                        break;
-                    case R.id.action_airplane2:
-                        setFrag(1);
-                        break;
-                    case R.id.action_airplane3:
-                        setFrag(2);
-                        break;
-                    case R.id.action_airplane4:
-                        setFrag(3);
-                        break;
-                    case R.id.action_airplane5:
-                        setFrag(4);
-                        break;
-                }
-                return true;
-            }
-        });
-        frag1 = new Frag1();
-        frag2 = new Frag2();
-        frag3 = new Frag3();
-        frag4 = new Frag4();
-        frag5 = new Frag5();
-
-        setFrag(0); //첫 프래그먼트 화면 선택
-    }
-
-    //프래그먼트 교체가 일어나는 switch case문
-    private void setFrag(int n){
-        fm = getSupportFragmentManager();
-        ft = fm.beginTransaction();
-
-        switch (n){
-            case 0:
-                ft.replace(R.id.main_frame,frag1);
-                ft.commit();
-                break;
-            case 1:
-                ft.replace(R.id.main_frame,frag2);
-                ft.commit();
-                break;
-            case 2:
-                ft.replace(R.id.main_frame,frag3);
-                ft.commit();
-                break;
-            case 3:
-                ft.replace(R.id.main_frame,frag4);
-                ft.commit();
-                break;
-            case 4:
-                ft.replace(R.id.main_frame,frag5);
-                ft.commit();
-                break;
-
-        }
     }
 }
