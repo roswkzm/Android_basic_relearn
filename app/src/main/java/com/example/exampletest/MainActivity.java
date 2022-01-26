4933b75f1d41aad34e33ff37548494fa5626df9e
이진영 <roswkzm@naver.com>
2022-01-26 16:34:00 +0900

    간단한 mp3 만들기

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -20,11 +21,66 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_play, btn_stop, btn_pause;
+    private MediaPlayer mediaPlayer;
+    private int length;
+
+    //엑티비티 완전 종료시
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_play = (Button) findViewById(R.id.btn_play);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
+        btn_pause = (Button) findViewById(R.id.btn_pause);
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.believe);
+
+        //재생버튼 클릭시
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+
+        //일시정지 눌럿을시
+        btn_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                    length = mediaPlayer.getCurrentPosition();
+                }else {
+                    mediaPlayer.seekTo(length);
+                    mediaPlayer.start();
+                }
+            }
+        });
+
+
+        //정지 눌렀을시
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                }
+            }
+        });
+
 
     }
 }
