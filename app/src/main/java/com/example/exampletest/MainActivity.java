20b5be102c97531d5b0a01a35e16eb59e06102b2
이진영 <roswkzm@naver.com>
2022-01-27 15:07:22 +0900

    프로젝트 초기화

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -1,38 +1,16 @@
 package com.example.exampletest;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_id, tv_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_id = (TextView) findViewById(R.id.tv_id);
-        tv_pass = (TextView) findViewById(R.id.tv_pass);
 
-        Intent intent = getIntent();
-        String userID = intent.getStringExtra("userID");
-        String userPass = intent.getStringExtra("userPass");
-
-        tv_id.setText(userID);
-        tv_pass.setText(userPass);
     }
 }
