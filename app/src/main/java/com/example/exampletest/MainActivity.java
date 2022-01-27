73f0f7b27dea4cf472e97b023e93c9d2cbadeb1f
이진영 <roswkzm@naver.com>
2022-01-27 16:51:15 +0900

    VideoView를 통해 풀스크린 비디오 틀기(가로방향)

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -3,34 +3,32 @@ package com.example.exampletest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.IntentFilter;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static TextView tv_state;
-    private NetworkReceiver receiver;
+    private VideoView videoView;
+    private MediaController mediaController;        // 재생이나 정지와 같은 미디어 제어 버튼 담당
+    private String videoURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_state = (TextView) findViewById(R.id.tv_state);
+        videoView = (VideoView) findViewById(R.id.videoView);
+        mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        Uri uri = Uri.parse(videoURL);
 
-        // 브로드 캐스트 리시버 등록
-        IntentFilter filter = new IntentFilter();
-        receiver = new NetworkReceiver();
-        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
-        registerReceiver(receiver,filter);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        //브롴드 캐스트 리시버 해제( 파괴할때 해제 안해주면 계속 실행함)
-        unregisterReceiver(receiver);
+        videoView.setMediaController(mediaController);  // 미디어 제어 버튼부분 세팅
+        videoView.setVideoURI(uri); //비디오의 주소 설정
+        videoView.start();
     }
 }
