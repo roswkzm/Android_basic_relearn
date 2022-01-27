c57414a750301ab9720e1fb339ecd921cdaed55f
이진영 <roswkzm@naver.com>
2022-01-27 16:32:42 +0900

    BroadcastRecevier을 이용해서 기기의 상태값 받기

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.exampletest;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static TextView tv_state;
+    private NetworkReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv_state = (TextView) findViewById(R.id.tv_state);
 
+        // 브로드 캐스트 리시버 등록
+        IntentFilter filter = new IntentFilter();
+        receiver = new NetworkReceiver();
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        registerReceiver(receiver,filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //브롴드 캐스트 리시버 해제( 파괴할때 해제 안해주면 계속 실행함)
+        unregisterReceiver(receiver);
     }
 }
