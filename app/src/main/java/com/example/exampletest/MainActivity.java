0e5982978db3687b41a3c8478be4b7488869dcb4
이진영 <roswkzm@naver.com>
2022-01-27 14:46:24 +0900

    PHP 서버를 통한 로그인&회원가입

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -18,11 +18,21 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tv_id, tv_pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv_id = (TextView) findViewById(R.id.tv_id);
+        tv_pass = (TextView) findViewById(R.id.tv_pass);
 
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+        String userPass = intent.getStringExtra("userPass");
+
+        tv_id.setText(userID);
+        tv_pass.setText(userPass);
     }
 }
