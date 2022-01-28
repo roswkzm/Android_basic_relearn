6d944c4e8aab9e3fbbe622e6b9282873208a73ff
이진영 <roswkzm@naver.com>
2022-01-28 20:21:25 +0900

    RadioButton 구현 RadioButton은 다중선택 불가 CheckBox는 다중선택 가능

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -6,16 +6,53 @@ import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup rg_gender;
+    private RadioButton rb_man, rb_woman;
+    private Button btn_result;
+    private String str_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rg_gender = (RadioGroup) findViewById(R.id.rg_gender);
+        rb_man = (RadioButton) findViewById(R.id.rb_man);
+        rb_woman = (RadioButton) findViewById(R.id.rb_woman);
+        btn_result = (Button) findViewById(R.id.btn_result);
 
+        rg_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() { //라디오 버튼들의 상태 값의 변경값을 감지함
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == R.id.rb_man){
+                    Toast.makeText(MainActivity.this, "남자 라디오 버튼", Toast.LENGTH_SHORT).show();
+                    str_result = rb_man.getText().toString();   // 라디오 버튼의 텍스트값을 담는다
+                }else if (i==R.id.rb_woman){
+                    Toast.makeText(MainActivity.this, "여자 라디오 버튼", Toast.LENGTH_SHORT).show();
+                    str_result = rb_woman.getText().toString();   // 라디오 버튼의 텍스트값을 담는다
+                }
+            }
+        });
+
+        btn_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (str_result != null){
+                    Toast.makeText(MainActivity.this, str_result, Toast.LENGTH_SHORT).show();
+                }else{  // str_result가 빈 값일 경우
+                    Toast.makeText(MainActivity.this, "라디오 버튼을 선택해주세요.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
