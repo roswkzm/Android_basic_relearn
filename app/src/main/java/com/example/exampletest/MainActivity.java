e134c62a322164285e22249b93f4038487068918
이진영 <roswkzm@naver.com>
2022-01-29 19:58:25 +0900

    프로젝트 초기화

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -1,39 +1,15 @@
 package com.example.exampletest;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
-import com.example.exampletest.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        // 엑티비티 바인딩 객체에 할당 및 뷰 설정 완료
-        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = mBinding.getRoot();
-        setContentView(view);
-
-        mBinding.tvHello.setText("이진영님 안녕하세요.");
-        mBinding.btnHello.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"안녕하세욤",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        //프래그먼트 교체행위
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.main_frame, new TestFragment());
-        ft.commit();
     }
 }
