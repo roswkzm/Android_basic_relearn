73538434a4c92757b675c0afd1bc5c01f53e46ea
이진영 <roswkzm@naver.com>
2022-01-29 13:52:37 +0900

    CheckBox 구현(다중선택가능)

--- app/src/main/java/com/example/exampletest/MainActivity.java
+++ app/src/main/java/com/example/exampletest/MainActivity.java
@@ -8,50 +8,46 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RadioGroup rg_gender;
-    private RadioButton rb_man, rb_woman;
+    private CheckBox chk_red, chk_blue, chk_green;
+    private TextView tv_result;
     private Button btn_result;
-    private String str_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rg_gender = (RadioGroup) findViewById(R.id.rg_gender);
-        rb_man = (RadioButton) findViewById(R.id.rb_man);
-        rb_woman = (RadioButton) findViewById(R.id.rb_woman);
-        btn_result = (Button) findViewById(R.id.btn_result);
+        chk_red = findViewById(R.id.chk_red);
+        chk_blue = findViewById(R.id.chk_blue);
+        chk_green = findViewById(R.id.chk_green);
+        tv_result = findViewById(R.id.tv_result);
+        btn_result = findViewById(R.id.btn_result);
 
-        rg_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() { //라디오 버튼들의 상태 값의 변경값을 감지함
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                if (i == R.id.rb_man){
-                    Toast.makeText(MainActivity.this, "남자 라디오 버튼", Toast.LENGTH_SHORT).show();
-                    str_result = rb_man.getText().toString();   // 라디오 버튼의 텍스트값을 담는다
-                }else if (i==R.id.rb_woman){
-                    Toast.makeText(MainActivity.this, "여자 라디오 버튼", Toast.LENGTH_SHORT).show();
-                    str_result = rb_woman.getText().toString();   // 라디오 버튼의 텍스트값을 담는다
-                }
-            }
-        });
-
-        btn_result.setOnClickListener(new View.OnClickListener() {
+        btn_result.setOnClickListener(new View.OnClickListener() {      //선택완료 버튼 눌렀을시
             @Override
             public void onClick(View view) {
-                if (str_result != null){
-                    Toast.makeText(MainActivity.this, str_result, Toast.LENGTH_SHORT).show();
-                }else{  // str_result가 빈 값일 경우
-                    Toast.makeText(MainActivity.this, "라디오 버튼을 선택해주세요.", Toast.LENGTH_SHORT).show();
+                String str_result = "";     // 버튼 눌렀을때마다 결과값 초기화
+                if (chk_red.isChecked()){
+                    str_result = str_result + chk_red.getText().toString();
                 }
+                if (chk_blue.isChecked()){
+                    str_result = str_result + chk_blue.getText().toString();
+                }
+                if (chk_green.isChecked()){
+                    str_result = str_result + chk_green.getText().toString();
+                }
+
+                tv_result.setText(str_result);
             }
         });
     }
